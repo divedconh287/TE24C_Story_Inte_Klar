cd0136cabf6d7a0d378e5f58c7248220a751fa45
Aaron Granath TE24C <divedconh287@gmail.com>
2025-09-06 21:30:08 +0200

    fixade efter plocka upp nyckel grejen

--- TE24C_Story/Program.cs
+++ TE24C_Story/Program.cs
@@ -8,81 +8,89 @@ Console.WriteLine($"{namn} går in i ett rum, höger eller vänster");
     {
     HogerEllerVanster = Console.ReadLine().ToLower();
 
-        if (HogerEllerVanster == "höger")
-        {
+    if (HogerEllerVanster == "höger")
+    {
         Console.WriteLine($"{namn} kollar åt höger, det sitter en spegel på väggen men det står också en låda gjord av kartong i hörnet, vill du kolla in i spegeln eller kika i lådan?");
-            string SpegelnEllerLadan = "";
-            while (SpegelnEllerLadan != "spegeln" && SpegelnEllerLadan != "lådan")
+        string SpegelnEllerLadan = "";
+        while (SpegelnEllerLadan != "spegeln" && SpegelnEllerLadan != "lådan")
+        {
+            SpegelnEllerLadan = Console.ReadLine().ToLower();
+
+            if (SpegelnEllerLadan == "spegeln")
+            { Console.WriteLine($"{namn} tittar in i spegeln som om de förväntar sig en komplimang, men spegeln säger ingenting."); }
+
+            else if (SpegelnEllerLadan == "lådan")
             {
-                SpegelnEllerLadan = Console.ReadLine().ToLower();
+                Console.WriteLine($"{namn} börjar gräva i lådan & hittar en nyckel, vill {namn} spara nyckeln?");
+                string Nyckel = "";
+                while (Nyckel != "ja" && Nyckel != "nej")
+                {
+                    Nyckel = Console.ReadLine().ToLower();
 
-                if (SpegelnEllerLadan == "spegeln")
-                { Console.WriteLine($"{namn} tittar in i spegeln som om de förväntar sig en komplimang, men spegeln säger ingenting."); }
+                    if (Nyckel == "ja")
+                    { Console.WriteLine($"{namn} tar nyckeln & lägger den i fickan, den är guld färgad & ganska dammig."); }
 
-                    else if (SpegelnEllerLadan == "lådan")
+                    else if (Nyckel == "nej")
+                    { Console.WriteLine($"{namn} lämnar nyckeln ifred & reser sig upp."); }
+
+                    else
+                    { Console.WriteLine("det är en ja eller nej fråga!"); }
+                }
+            }
+            else
+            { Console.WriteLine("Spegeln eller lådan?"); }
+        }
+    }
+
+    else if (HogerEllerVanster == "vänster")
+    {
+        Console.WriteLine($"{namn} kollar åt vänster & ser en stor säng & en fåtölj, ska du lägga dig i sängen eller fåtöljen?");
+        string SangEllerFatolj = "";
+        while (SangEllerFatolj != "sängen" && SangEllerFatolj != "fåtöljen")
+        {
+            SangEllerFatolj = Console.ReadLine().ToLower();
+
+            if (SangEllerFatolj == "sängen")
+            { Console.WriteLine($"{namn} lägger sig i sängen & somnar."); }
+
+            else if (SangEllerFatolj == "fåtöljen")
+            {
+                Console.WriteLine($"{namn} sätter sig i fåtöljen & märker att det finns något som blänker genom handtaget av kartongen på andra sidan av rummet.");
+                string Nyckel0 = "";
+                while (Nyckel0 != "hämta" && Nyckel0 != "lämna")
+                {
+                    Nyckel0 = Console.ReadLine().ToLower();
+                    if (Nyckel0 == "hämta")
                     {
-                    Console.WriteLine($"{namn} börjar gräva i lådan & hittar en nyckel, vill {namn} spara nyckeln?");
-                            string Nyckel = "";
-                            while (Nyckel != "ja" && Nyckel != "nej")
-                            {
-                                Nyckel = Console.ReadLine().ToLower();
+                        Console.WriteLine($"{namn} ställer sig upp & hämtar nyckeln.");
 
-                                if (Nyckel == "ja")
-                                { Console.WriteLine($"{namn} tar nyckeln & lägger den i fickan, den är guld färgad & ganska dammig."); }
-
-                                else if (Nyckel == "nej")
-                                { Console.WriteLine($"{namn} lämnar nyckeln ifred & reser sig upp."); }
-
-                                else
-                                { Console.WriteLine("det är en ja eller nej fråga!"); }
-                            }
+                        Console.WriteLine("bla");
+                        string l = "";
+                        while (l != "n" && l != "m")
+                        {
+                            l = Console.ReadLine().ToLower();
+                            if (l == "n")
+                            { Console.WriteLine($"{namn} bla"); }
+                            else if (l == "m")
+                            { Console.WriteLine($"{namn} bla"); }
+                            else
+                            { Console.WriteLine("n eller m?"); }
                         }
-                else
-                { Console.WriteLine("Spegeln eller lådan?"); }
+                    }
+
+                    else if (Nyckel0 == "lämna")
+                    { Console.WriteLine($"{namn} "); }
+
+                    else
+                    { Console.WriteLine("tänker du bara sitta där?"); } 
             }
         }
-
-        else if (HogerEllerVanster == "vänster")
-        {
-            Console.WriteLine($"{namn} kollar åt vänster & ser en stor säng & en fåtölj, ska du lägga dig i sängen eller fåtöljen?");
-                string SangEllerFatolj = "";
-                while (SangEllerFatolj != "sängen" && SangEllerFatolj != "fåtöljen")
-                {
-                SangEllerFatolj = Console.ReadLine().ToLower();
-
-                if (SangEllerFatolj == "sängen")
-                { Console.WriteLine($"{namn} lägger sig i sängen & somnar."); }
-
-                    else if (SangEllerFatolj == "fåtöljen")
-                    {
-                    Console.WriteLine($"{namn} sätter sig i fåtöljen & märker att det finns något som blänker genom handtaget av lådan ");
-                        string Nyckel0 = "";
-                        while (Nyckel0 != "hämta" && Nyckel0 != "lämna")
-                        {
-                            if (Nyckel0 == "hämta")
-                            { Console.WriteLine($"{namn} ställer sig upp & hämtar nyckeln."); }
-
-                                else if (Nyckel0 == "lämna")
-                                { Console.WriteLine($"{namn} sätter sig i fåtöljen & märker att det finns något som blänker genom handtaget av lådan "); }
-
-                                    else
-                                    { Console.WriteLine("tänker du bara sitta där?"); }
-                        }   
-                    }
-                else
-                { Console.WriteLine("sängen eller fåtöljen?"); }
-                }
+            else
+        { Console.WriteLine("sängen eller fåtöljen?"); }
+        }
         }
 
         else
         { Console.WriteLine("höger eller vänster?"); }
     }
-        
-
-
-                                                        
-
-
-        
-
     Console.ReadLine();
